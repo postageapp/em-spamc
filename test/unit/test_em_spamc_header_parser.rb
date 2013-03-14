5d448ec02846fdd6dafc3d8fdb9e220a8a40f89a
Stephan Leroux <stephanleroux@gmail.com>
2013-03-14 16:12:21 -0400

    Removed unused test messages. Made messages safer

--- test/unit/test_em_spamc_header_parser.rb
+++ test/unit/test_em_spamc_header_parser.rb
@@ -2,14 +2,14 @@ require_relative '../helper'
 
 class TestEmSpamcHeaderParser < Test::Unit::TestCase
   def test_score_header
-    report = example_report('spam-003-report')
+    report = example_report('spam-report')
     headers = EmSpamc::HeaderParser.parse(report)
     assert_equal headers[:spam], true
     assert headers[:score] >= 0
   end
 
   def test_zero_header
-    report = example_report('zero-001-report')
+    report = example_report('zero-report')
     headers = EmSpamc::HeaderParser.parse(report)
     assert headers[:score]
     assert_equal headers[:score], 0
--- test/unit/test_em_spamc_report_parser.rb
+++ test/unit/test_em_spamc_report_parser.rb
@@ -3,7 +3,7 @@ require_relative '../helper'
 class TestEmSpamcReportParser < Test::Unit::TestCase
   def test_parsing_spam
     eventmachine do
-      report = example_report('spam-003-report')
+      report = example_report('spam-report')
       rules = EmSpamc::ReportParser.parse(report)
       assert rules
     end
@@ -11,7 +11,7 @@ class TestEmSpamcReportParser < Test::Unit::TestCase
 
   def test_parsing_zero
     eventmachine do
-      report = example_report('zero-001-report')
+      report = example_report('zero-report')
       rules = EmSpamc::ReportParser.parse(report)
       assert rules
       assert_equal rules, []
